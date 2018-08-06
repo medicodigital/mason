cfe812c871359c018b7ae5a51c9f7f1591a0ed1e
djc74 <davidcooper974@gmail.com>
2018-08-06 08:22:24 +0100

    IngaJ started

--- src/components/Osteopaths.js
+++ src/components/Osteopaths.js
@@ -9,52 +9,51 @@ import inga from '../images/therapists/inga-head.jpeg'
 import vincent from '../images/therapists/vincent-martino.jpeg'
 import heloise from '../images/therapists/Héloïse-Gilleman-3.jpg'
 
-
 const OsteopathsWrapper = styled.div`
-    align-items: center;
-    background: #86aecf;
-    color: #fff;
+  align-items: center;
+  background: #86aecf;
+  color: #fff;
+  display: flex;
+  height: 55px;
+  margin: 0 auto;
+  position: relative;
+  width: 100vw;
+  @media (min-width: 600px) {
+    max-width: 60%;
+  }
+  > div {
+    margin: 0 5%;
+  }
+`
+
+const OsteopathsContainer = styled.div`
+  input {
+    display: none;
+  }
+  input + label {
+    position: inherit;
+    > div {
+      margin-left: 5%;
+    }
+  }
+  label {
     display: flex;
-    height: 55px;
-    margin: 0 auto;
-    position: relative;
-    width: 100vw;
-    @media (min-width: 600px) {
-        max-width: 60%;
+    width: 300px;
+    :hover {
+      cursor: pointer;
     }
+    > p {
+      font-size: 1.7rem;
+      margin-left: 10px;
+    }
+  }
+  input:checked + label {
     > div {
-        margin: 0 5%;
+      transform: rotate(90deg);
+      transition: 0.5s;
     }
-    `
-    
-    const OsteopathsContainer = styled.div`
-    input {
-        display: none;
-    }
-    input + label {
-        position: inherit;
-        > div {
-            margin-left: 5%;
-        }
-    }
-    label {
-        display:flex;
-        width: 300px;
-        :hover {
-            cursor: pointer;
-        }
-        > p {
-            font-size: 1.7rem;
-            margin-left: 10px;
-        }
-    }
-    input:checked + label {
-    > div {
-        transform: rotate(90deg); 
-        transition: 0.5s;
-    }
-}
-input ~ nav {
+  }
+  input ~ nav {
     left: 0;
     position: absolute;
     top: 35px;
@@ -63,92 +62,99 @@ input ~ nav {
     width: 100%;
     z-index: 3;
     > ul {
-        padding: 0;
-        right: 0;
-        width: 100%;
-        > li {
-            background: #86aecf;
-            display: block;
-            color: #fff;
-            opacity: 0;
-            padding: 10px;
-            transition: 0.5s;
-            text-decoration: none;
-            :last-of-type {
-                padding-bottom: 20px;
-            }
+      padding: 0;
+      right: 0;
+      width: 100%;
+      > li {
+        background: #86aecf;
+        display: block;
+        color: #fff;
+        opacity: 0;
+        padding: 10px;
+        transition: 0.5s;
+        text-decoration: none;
+        :last-of-type {
+          padding-bottom: 20px;
         }
-        
+      }
     }
-}
-input:checked ~ nav {
+  }
+  input:checked ~ nav {
     height: 100%;
     > ul {
-        > li {
-            opacity: 1;
-            transition: 0.5s;
-        }
+      > li {
+        opacity: 1;
+        transition: 0.5s;
+      }
     }
-}
+  }
 `
 
 const ImageHolder = styled.div`
-    display: flex;
-    font-size: 1.7rem;
-    text-align: left;
-    img {
-        height: 80px;
-        margin-left: 15%;
-        margin-right: 10px;
-    }
+  display: flex;
+  font-size: 1.7rem;
+  text-align: left;
+  img {
+    height: 80px;
+    margin-left: 15%;
+    margin-right: 10px;
+  }
 `
 
 const Osteopaths = () => (
-    <OsteopathsWrapper>
-        <OsteopathsContainer>
-        <input id="osteos" type="checkbox" />
-        <label htmlFor="osteos">
-        <Icon size={25} icon={ arrowRightThin } />
+  <OsteopathsWrapper>
+    <OsteopathsContainer>
+      <input id="osteos" type="checkbox" />
+      <label htmlFor="osteos">
+        <Icon size={25} icon={arrowRightThin} />
         <p>Our osteopaths</p>
-        </label>
-        <nav>
-            <ul>
-                <li>
-                    <ImageHolder>
-                        <img src={andrew} alt="Andrew headshot" />
-                        <Link to="/therapistAndrew/">
-                        Andrew<br></br>Allen
-                        </Link>
-                    </ImageHolder>
-                </li>
-                <li>
-                    <ImageHolder>
-                        <img src={inga} alt="Inga headshot" />
-                        <Link to="/therapistInga/">
-                        Inga<br></br>Werthmann
-                        </Link>
-                    </ImageHolder>
-                </li>
-                <li> 
-                    <ImageHolder>
-                        <img src={vincent} alt="Vincent headshot" />
-                        <Link to="/therapistVincent/">
-                        Vincent<br></br>Martino
-                        </Link>
-                    </ImageHolder>
-                </li>
-                <li>
-                    <ImageHolder>
-                        <img src={heloise} alt="Heloise headshot" />
-                        <Link to="/therapistHeloise/">
-                        Héloïse<br></br>Gilleman
-                        </Link>
-                    </ImageHolder>
-                </li>
-            </ul>
-        </nav>
-        </OsteopathsContainer>
-    </OsteopathsWrapper>
+      </label>
+      <nav>
+        <ul>
+          <li>
+            <ImageHolder>
+              <img src={andrew} alt="Andrew headshot" />
+              <Link to="/therapistAndrew/">
+                Andrew<br />Allen
+              </Link>
+            </ImageHolder>
+          </li>
+          <li>
+            <ImageHolder>
+              <img src={inga} alt="Inga headshot" />
+              <Link to="/therapistInga/">
+                Inga<br />Werthmann
+              </Link>
+            </ImageHolder>
+          </li>
+          <li>
+            <ImageHolder>
+              <img src={vincent} alt="Vincent headshot" />
+              <Link to="/therapistVincent/">
+                Vincent<br />Martino
+              </Link>
+            </ImageHolder>
+          </li>
+          <li>
+            <ImageHolder>
+              <img src={heloise} alt="Heloise headshot" />
+              <Link to="/therapistHeloise/">
+                Héloïse<br />Gilleman
+              </Link>
+            </ImageHolder>
+          </li>
+          <li>
+            <ImageHolder>
+              <img src={heloise} alt="IngaJ headshot" />
+              <Link to="/therapistIngaJ/">
+                Inga<br />Jakubovskaja
+              </Link>
+            </ImageHolder>
+          </li>
+        </ul>
+      </nav>
+    </OsteopathsContainer>
+  </OsteopathsWrapper>
 )
 
 export default Osteopaths
---
+++ src/pages/therapistIngaJ.js
@@ -0,0 +1,111 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import TherapyHeader from '../components/TherapyHeader'
+import Links from '../components/Links'
+
+import inga from '../images/therapists/inga-head.jpeg'
+
+const TherapistWrapper = styled.div`
+  background: #ddeaf6;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`
+const ImageBlurbContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 600px) {
+    order: 2;
+    padding: 5vh 10vw;
+  }
+  @media (min-width: 900px) {
+    align-items: center;
+    flex-direction: row;
+    justify-content: center;
+    padding: 10vh 10vw;
+  }
+`
+
+const ImageHolder = styled.div`
+  align-self: flex-start;
+  margin: 20px auto;
+  width: 60vw;
+  > img {
+    width: 100%;
+  }
+  @media (min-width: 900px) {
+    margin: 0 20px 0 0;
+    width: 30%;
+  }
+`
+
+const BlurbHolder = styled.div`
+  margin: 0 10%;
+  @media (min-width: 600px) {
+    margin: 0;
+  }
+  @media (min-width: 900px) {
+    max-width: 30vw;
+  }
+  > h1,
+  h3 {
+    margin: 0;
+    text-align: center;
+  }
+  > h1 {
+    font-weight: 700;
+  }
+  > h3 {
+    font-weight: 400;
+  }
+`
+
+const StickyHolder = styled.div`
+  background: #86aecf;
+  color: #fff;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  @media (min-width: 600px) {
+    order: 1;
+  }
+`
+
+const TherapistIngaJ = () => (
+  <TherapistWrapper>
+    <TherapyHeader therapy="osteopathy" />
+    <ImageBlurbContainer>
+      <ImageHolder>
+        <img src={inga} alt="IngaJ headshot" />
+      </ImageHolder>
+      <BlurbHolder>
+        <h1>Inga Jakubovskaja</h1>
+        <h3>BSc &#40;Hons&#41; Osteopathy, DO</h3>
+        <p>
+          Inga graduated from the European School of Osteopathy with an
+          Integrated Master’s Degree. Every patient will note the smile on her
+          face and a friendly handshake when meeting her. Inga strongly believes
+          that communication is the key to any treatment and with patient and
+          practitioner interaction, using her varied skills learned throughout
+          her career she meets all needs proposed. With honed skills and desire
+          in osteopathy, she displays profession, ability, diagnostics and
+          treatment for all patients from different backgrounds, ages and
+          finally provides tailor-made management plans. Expanding her knowledge
+          Inga has a particular interest in paediatric and maternity osteopathy
+          and with these experiences, her joy of treating others heightens.
+          Other than osteopathy Inga holds accreditation in Classic Massage and
+          Kinesiology Taping. Yet if Inga isn’t practising in the clinic, she’s
+          making chain maille jewellery to which each piece is custom, keeping
+          fit with her beloved Siberian Husky who puts her through her paces. In
+          the evenings she loves to read and immerse herself in a good book.
+        </p>
+      </BlurbHolder>
+    </ImageBlurbContainer>
+    <StickyHolder>
+      <Links />
+    </StickyHolder>
+  </TherapistWrapper>
+)
+
+export default TherapistIngaJ
