90586e5ca6e5b5e711273d652e7e5868e4afe7e2
djc74 <davidcooper974@gmail.com>
2018-08-09 23:10:34 +0100

    Privacy page finished

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -10,9 +10,227 @@ const PrivacyWrapper = styled.div`
   margin: 0;
 `
 
+const PrivacyHolder = styled.div`
+  margin: 20px;
+  h3 {
+    margin-bottom: 0;
+  }
+  p {
+    margin-top: 5px;
+    margin-bottom: 0;
+  }
+`
+
 const PrivacyPage = () => (
   <PrivacyWrapper>
-    <TherapyHeader therapy="Privacy Statement" />
+    <TherapyHeader therapy="Privacy" />
+    <PrivacyHolder>
+      <h3>GDPR 2018 Privacy Notice</h3>
+      <p>
+        This document refers to personal data, which is defined as information
+        concerning any living person (a natural person who hereafter will be
+        called the Data Subject) that is not already in the public domain. The
+        General Data Protection Regulation (GDPR) which is EU wide and far more
+        extensive than its predecessor the Data Protection Act, along with the
+        Privacy and Electronic Communications Regulations (PECR), seek to
+        protect and enhance the rights of EU data subjects. These rights cover
+        the safeguarding of personal data, protection against the unlawful
+        processing of personal data and the unrestricted movement of personal
+        data within the EU and its storage within the EEA.{' '}
+      </p>
+      <p>
+        1 – The Mason Clinic, based at 1b Cornwallis Avenue, Chatham, Kent,
+        ME46JT, which hereafter for the purposes of this Privacy Notice will be
+        referred to as the Mason Clinic, is pleased to provide the following
+        information:
+      </p>
+      <p>
+        {' '}
+        2 – Who we are At the Mason Clinic, we diagnose and treat health
+        conditions. Treatments are carried out in accordance with the Institute
+        of Osteopathy’s patient charter
+        http://www.iosteopathy.org/osteopathy/the-patient-charter/. The practice
+        also provides other treatments, about which our staff will be pleased to
+        provide more details.
+      </p>{' '}
+      <p>
+        3 – Personal Data a) For the purposes of providing treatment, The Mason
+        Clinic may require detailed medical information. We will only collect
+        what is relevant and necessary for your treatment. When you visit our
+        practice, we will make notes which may include details concerning your
+        medication, treatment and other issues affecting your health. This data
+        is always held securely, is not shared with anyone not involved in your
+        treatment, although for data storage purposes it may be handled by
+        pre-vetted staff who have all signed an integrity and confidentiality
+        agreement. To be able to process your personal data it is a condition of
+        any treatment that you give your explicit consent to allow the Mason
+        Clinic to document and process your personal medical data. Contact
+        details provided by you such as telephone numbers, email addresses,
+        postal addresses may be used to remind you of future appointments and
+        provide reports or other information concerning your treatment. As part
+        of our obligations as primary healthcare practitioners there may be
+        circumstances related to your treatment, on-going care or medical
+        diagnosis that will require the sharing of your medical records with
+        other healthcare practitioners e.g GPs, consultants, surgeons and/or
+        medical insurance companies. Where this is required we will always
+        inform you first unless we are under a legal obligation to comply. b)
+        For marketing purposes, the Mason Clinic may also use the contact
+        details provided by you to respond to your enquiries, including making
+        telephone contact and emailing information to you which the practice
+        believes may be of interest to you. c) In making initial contact with
+        the practice you consent to the Mason Clinic maintaining a marketing
+        dialogue with you until you either opt out (which you can do at any
+        time) or we decide to desist in promoting our services. The Mason Clinic
+        may occasionally also act on behalf of its patients in the capacity of
+        data processor, when we may promote other practitioners based at our
+        premises, who may not be employed by us. The Mason Clinic do not broker
+        your data and you can ask to be removed from our marketing database by
+        emailing or phoning the practice using the contact details provided at
+        the end of this Privacy Notice. d) Some basic personal data may be
+        collected about you from the marketing forms and surveys you complete,
+        from records of our correspondence and phone calls and details of your
+        visits to our website, including but not limited to, personally
+        identifying information like Internet Protocol (IP) addresses. e) The
+        Mason Clinics’ website uses cookies, which is a string of information
+        that a website stores on a visitor’s computer, and that the visitor’s
+        browser provides to the website each time the visitor returns.
+        WordPress.org uses cookies to help Osteopaths to identify and track
+        visitors and their website access preferences. Osteopaths’ website
+        visitors who do not wish to have cookies placed on their computers
+        should set their browsers to refuse cookies before using The Mason
+        Clinics’ website. f) The Mason Clinic will only collect the information
+        needed so that we can provide you with the services you require, the
+        business does not sell or broker your data.
+      </p>
+      <p>
+        4 – Legal basis for processing any personal data To meet our contractual
+        obligations obtained from explicit Patient Consent and legitimate
+        interest to respond to enquiries concerning the services provided.
+      </p>
+      <p>
+        {' '}
+        5 – Legitimate interests pursued by The Mason Clinic To promote
+        treatments for patients with all types of health problems indicated for
+        osteopathic care.
+      </p>
+      <p>
+        {' '}
+        6 – Consent Through agreeing to this privacy notice you are consenting
+        to The Mason Clinic processing your personal data for the purposes
+        outlined. You can withdraw consent at any time by using the postal,
+        email address or telephone number provided at the end of this Privacy
+        Notice.
+      </p>
+      <p>
+        {' '}
+        7 – Disclosure Osteopaths will keep your personal information safe and
+        secure, only staff engaged in providing your treatment will have access
+        to your patient records, although our administration team will have
+        access to your contact details so that they can make appointments and
+        manage your account. The Mason Clinic will not disclose your Personal
+        Information unless compelled to, in order to meet legal obligations,
+        regulations or valid governmental requests. The practice may also
+        enforce its Terms and Conditions, including investigating potential
+        violations of its Terms and Conditions to detect, prevent or mitigate
+        fraud or security or technical issues; or to protect against imminent
+        harm to the rights, property or safety of its staff.
+      </p>
+      <p>
+        {' '}
+        8 – Retention Policy The Mason Clinic will process personal data during
+        the duration of any treatment and will continue to store only the
+        personal data needed for eight years after the contract has expired to
+        meet any legal obligations. After eight years all personal data will be
+        deleted, unless basic information needs to be retained by us to meet our
+        future obligations to you, such as erasure details. Records concerning
+        minors who have received treatment will be retained until the child has
+        reached the age of 25.
+      </p>
+      <p>
+        {' '}
+        9 – Data storage All Data is held in the United Kingdom. The Mason
+        Clinic does not store personal data outside the EEA.
+      </p>
+      <p>
+        {' '}
+        10 – Your rights as a data subject At any point whilst The Mason Clinic
+        is in possession of, or processing your personal data, all data subjects
+        have the following rights: Right of access – you have the right to
+        request a copy of the information that we hold about you. Right of
+        rectification – you have a right to correct data that we hold about you
+        that is inaccurate or incomplete. Right to be forgotten – in certain
+        circumstances you can ask for the data we hold about you to be erased
+        from our records. Right to restriction of processing – where certain
+        conditions apply you have a right to restrict the processing. Right of
+        portability – you have the right to have the data we hold about you
+        transferred to another organisation. Right to object – you have the
+        right to object to certain types of processing such as direct marketing.
+        Right to object to automated processing, including profiling – you also
+        have the right not to be subject to the legal effects of automated
+        processing or profiling. In the event that The Mason Clinic refuses your
+        request under rights of access, we will provide you with a reason as to
+        why, which you have the right to legally challenge. At your request The
+        Mason Clinic can confirm what information it holds about you and how it
+        is processed.
+      </p>
+      <p>
+        {' '}
+        11 – You can request the following information: Identity and the contact
+        details of the person or organisation (Osteopaths) that has determined
+        how and why to process your data. Contact details of the data protection
+        officer, where applicable. The purpose of the processing as well as the
+        legal basis for processing. If the processing is based on the legitimate
+        interests of The Mason Clinic and information about these interests. The
+        categories of personal data collected, stored and processed.
+        Recipient(s) or categories of recipients that the data is/will be
+        disclosed to. How long the data will be stored. Details of your rights
+        to correct, erasure, restrict or object to such processing. Information
+        about your right to withdraw consent at any time. How to lodge a
+        complaint with the supervisory authority (ICO). Whether the provision of
+        personal data is a statutory or contractual requirement, or a
+        requirement necessary to enter into a contract, as well as whether you
+        are obliged to provide the personal data and the possible consequences
+        of failing to provide such data. The source of personal data if it
+        wasn’t collected directly from you. Any details and information of
+        automated decision making, such as profiling, and any meaningful
+        information about the logic involved, as well as the significance and
+        expected consequences of such processing.
+      </p>
+      <p>
+        {' '}
+        12 – To access what personal data is held, identification will be
+        required The Mason Clinic will accept the following forms of
+        identification (ID) when information on your personal data is requested:
+        a copy of your driving licence, passport, birth certificate and a
+        utility bill not older than three months. A minimum of one piece of
+        photographic ID listed above and a supporting document is required. If
+        The Mason Clinic is dissatisfied with the quality, further information
+        may be sought before personal data can be released. All requests should
+        be made to enquiries@masonclinic.co.uk or by phoning +44 (0) 1634 842583
+        or writing to us at the address further below.
+      </p>
+      <p>
+        13 Complaints In the event that you wish to make a complaint about how
+        your personal data is being processed by The Mason Clinic you have the
+        right to complain to us. If you do not get a response within 30 days,
+        you can complain to the ICO. The details for each of these contacts are:
+        Inga Werthmann, The Mason Clinic, 1b Cornwallis Avenue, Chatham, ME46JT
+        Telephone 01634 842583 or email: enquiries@masonclinic.co.uk
+      </p>{' '}
+      <p>ICO</p>
+      <p>
+        {' '}
+        Wycliffe House, Water Lane, Wilmslow, SK9 5AF Telephone +44 (0) 303 123
+        1113 or email: https://ico.org.uk/global/contact-us/email/
+      </p>{' '}
+      <p>
+        You have a right to receive a copy of the data/information we hold about
+        you or to authorise someone to act on your behalf. Please print out and
+        complete this form providing proof of your identity. Your request will
+        be processed within 30 calendar days on receipt of a fully completed
+        request form with proof of identity.
+      </p>
+    </PrivacyHolder>
   </PrivacyWrapper>
 )
 
